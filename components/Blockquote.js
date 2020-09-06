db774ff04b03c399c5b8928c7e1cc9b41f70dc13
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2020-09-06 19:35:39 +0200

    Adds links to social

--- components/Blockquote.js
+++ components/Blockquote.js
@@ -1,5 +1,5 @@
 export const Blockquote = ({ children }) => (
-  <blockquote className="w-full md:w-11/12 my-4 mx-auto relative px-4 text-xl italic border-l-4 bg-neutral-100  border-primary">
+  <blockquote className="w-full md:w-11/12 my-4 mx-auto relative px-4 text-base italic border-l-4 bg-neutral-100  border-primary-lighter">
     <div className="mb-4">{children}</div>
   </blockquote>
 );
