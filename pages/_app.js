849d2e11fbe647b1c636f2d8ba5c7c977f6c5aa6
Nicolas <ntorres.dev@gmail.com>
2021-06-30 15:29:42 +0200

    Adds gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
 import '../tailwind.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-N2M6MS6' });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
